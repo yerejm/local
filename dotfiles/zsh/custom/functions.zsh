7b233bcce7b2d91b0feafb15f731790efc3c5aa1
yerejm <github@thelostyerejm.com>
2015-07-26 18:07:36 +1000

    zsh: add procfile and procport to find processes holding files and ports

--- dotfiles/zsh/custom/functions.zsh
+++ dotfiles/zsh/custom/functions.zsh
@@ -1,7 +1,25 @@
 function col {
   awk "{print \$$1}"
 }
+
 function skip {
   n=$(($1 + 1))
   cut -d' ' -f$n-
 }
+
+# process listening on port
+function procport {
+  service="4TCP"
+  [[ -n "$1" ]] && service="${service}:$1"
+  sudo lsof -n "-i${service}" | grep LISTEN
+}
+
+# file handles held by an open process
+function procfile {
+  lsof -p "$1" | col 9 | sort | uniq | while read -r file; do
+    if test -f "${file}"; then
+      echo "${file}"
+    fi
+  done
+}
+
