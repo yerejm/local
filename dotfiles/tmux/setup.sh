918121bc67ee314ba47bddca5779bfd8a7b2ccb1
yerejm <>
2021-02-19 18:16:10 +1000

    tmux: move tmux config under .config

--- dotfiles/tmux/setup.sh
+++ dotfiles/tmux/setup.sh
@@ -6,10 +6,9 @@ set -o pipefail
 
 CURDIR=$(dirname "$(pwd)/${0}")
 
-# install dotfile
-ln -f -s "${CURDIR}/tmux.conf" "${HOME}/.tmux.conf"
+ln -f -s "$(pwd)" "${HOME}/.config/tmux"
 
-TMUX_TPM_DIR="${HOME}/.tmux/plugins/tpm"
+TMUX_TPM_DIR="${HOME}/.config/tmux/plugins/tpm"
 if [ -e "${TMUX_TPM_DIR}" ]; then
   echo "Updating tmux tpm" && (cd "${TMUX_TPM_DIR}" && git pull)
 else
