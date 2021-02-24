77ab0d0fbeb3cf995aa8de0a91e2a21823b847af
yerejm <>
2021-02-24 21:37:06 +1000

    support linux

--- linux/setup.sh
+++ linux/setup.sh
@@ -4,23 +4,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get -y install \
-  zsh \
-  unzip \
-  unrar \
-  tmux \
-  vim \
-  exuberant-ctags \
-  silversearcher-ag \
-  git \
-  mercurial \
-  cmake \
-  shellcheck \
-  valgrind \
-  ansible \
-  vagrant \
-  virtualbox-qt \
-  redshift-gtk
-
+sudo dnf install -y \
+	ulauncher \
+	plank \
+	fd-find \
+	fzf \
+	zsh \
+	unzip \
+	tmux \
+	vim \
+	ctags \
+	git \
+	mercurial \
+	cmake \
+	ShellCheck \
+	valgrind \
+	ansible \
+	vagrant \
+	patch \
+	gcc \
+	g++ \
+	make \
+	ninja-build
