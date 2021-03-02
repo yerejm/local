b903df19c03d8ace78c96dd3c5c8aaaff1518ae7
yerejm <>
2021-03-02 23:07:09 +1000

    handle debian and fedora

--- linux/setup.sh
+++ linux/setup.sh
@@ -4,30 +4,58 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-sudo dnf install -y \
-	ulauncher \
-	plank \
-	fd-find \
-	fzf \
-	zsh \
-	unzip \
-	tmux \
-	vim \
-	ctags \
-	git \
-	mercurial \
-	cmake \
-	ShellCheck \
-	valgrind \
-	ansible \
-	vagrant \
-	patch \
-	gcc \
-	g++ \
-	make \
-	ninja-build
+dist=$(grep ^ID= /etc/os-release | cut -c 4-)
+case $dist in
+    fedora) sudo dnf install -y \
+        ulauncher \
+        plank \
+        fd-find \
+        fzf \
+        zsh \
+        unzip \
+        tmux \
+        vim \
+        ctags \
+        git \
+        mercurial \
+        cmake \
+        ShellCheck \
+        valgrind \
+        ansible \
+        vagrant \
+        patch \
+        gcc \
+        g++ \
+        make \
+        ninja-build
+        ;;
+    debian) sudo apt install -y \
+        plank \
+        fd-find \
+        fzf \
+        zsh \
+        unzip \
+        tmux \
+        vim \
+        universal-ctags \
+        git \
+        mercurial \
+        cmake \
+        shellcheck \
+        valgrind \
+        ansible \
+        vagrant \
+        patch \
+        gcc \
+        g++ \
+        make \
+        ninja-build
+        ;;
+    *) >&2 echo "Unsupported distribution $dist"
+        ;;
+esac
 
 # brew ol' buddy, don't let me down...
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install \
+/home/linuxbrew/.linuxbrew/bin/brew install \
     ripgrep-all
